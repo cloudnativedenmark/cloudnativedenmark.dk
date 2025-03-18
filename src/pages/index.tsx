e73f7ab1759f13f84d00f4caea2a97e1c7d9ae6e
Thor Anker Kvisgård Lange <lange@kvisgaard-lange.dk>
2025-03-18 20:33:59 +0100

    feat: Adding ticket button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,6 +26,16 @@ const IndexPage: React.FC<PageProps> = () => {
               <p className="mt-8 text-lg font-medium text-pretty text-primary sm:text-xl/8">
                 KCD Denmark will return as <b>Cloud Native Denmark</b> in 2025.
               </p>
+              <p className="mt-10">
+                {/* px-8 py-[18px] text-lg --- px-8 py-[18px] text-lg  */}
+                <a
+                  className="bg-blue-900 text-white hover:bg-blue-700 h-14 rounded-3xl bg-red px-20 py-2 text-xl font-bold leading-none shadow-[0_15px_40px_#999999] hover:shadow-[0px_15px_30px_#adadad]"
+                  href="https://cloudnativedenmark.ticketbutler.io/en/e/cloud-native-denmark/"
+                  target="_blank"
+                >
+                  Get your ticket
+                </a>
+              </p>
             </div>
           </div>
         </div>
