c4d9e54e17ef7ada7adad3bf1409329c0a8101e9
Thor Anker Kvisgård Lange <tal@netic.dk>
2025-03-30 20:26:58 +0200

    feat: Sponsor section (#13)
    * feat: Adding simple 'Sponsors' section to index page
    
    * feat: Fiddling with rendering sponsors
    
    * feat: Minor adjustments - first community sponsors added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,31 @@
 import * as React from "react";
-import type { HeadFC, PageProps } from "gatsby";
+import { type HeadFC, type PageProps, graphql } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Logo from "../images/logo.svg";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
-const IndexPage: React.FC<PageProps> = () => {
+interface Sponsor {
+  title: string;
+  url: string;
+  scale: string;
+  logo: {
+    publicURL: string;
+  };
+}
+
+type DataProps = {
+  allCommunityYaml: {
+    nodes: Sponsor[];
+  };
+};
+
+const IndexPage: React.FC<PageProps<DataProps>> = ({
+  data: { allCommunityYaml },
+}) => {
   return (
     <Layout>
+      {/* HERO */}
       <section className="bg-background">
         <div className="mx-auto max-w-6xl">
           <div className="flex flex-row px-6 pt-14 py-24">
@@ -46,6 +65,67 @@ const IndexPage: React.FC<PageProps> = () => {
           exciting talks and plenty of opportunities for growing and networking.
         </p>
       </section>
+      {/* HERO */}
+
+      {/* SPONSORS */}
+      <section className="safe-paddings relative bg-white sm:pb-16">
+        <div className="mx-auto max-w-6xl text-center text-primary">
+          <h2 className="text-6xl font-bold leading-denser" id="sponsors">
+            Sponsors
+          </h2>
+          <p className="text-lg font-medium text-pretty text-primary sm:text-xl/8 mx-auto mt-5 max-w-[800px] leading-normal">
+            If you’re interested in becoming a sponsor, please contact&nbsp;
+            <a
+              className="font-semibold text-blue-900"
+              href="mailto:contact@cloudnativedenmark.dk"
+            >
+              sponsor@cloudnativedenmark.dk
+            </a>
+            &nbsp;or view our&nbsp;
+            <a
+              className="font-semibold text-blue-900"
+              href="https://www.canva.com/design/DAF5X4WiO6I/6bgxDDraWrQzTfyTYtINmQ/view"
+              target="_blank"
+            >
+              prospectus
+            </a>
+          </p>
+        </div>
+        <ul className="mt-16">
+          <li>
+            <p className="text-center text-2xl font-semibold uppercase leading-normal text-primary-1">
+              Community Sponsors
+            </p>
+            <ul className="mt-10 mb-[70px] flex flex-wrap justify-center gap-x-8 xl:gap-y-6">
+              {allCommunityYaml.nodes.map((sponsor, index) => {
+                return (
+                  <li
+                    key={index}
+                    className="flex items-center justify-center min-w-[384px] min-h-[122px] sm:min-w-[320px] sm:min-h-[115px] xs:min-w-fit xs:max-w-full"
+                  >
+                    <a
+                      href={sponsor.url}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="flex h-full w-fit items-center justify-center"
+                    >
+                      {sponsor.logo && (
+                        <img
+                          className="h-auto max-h-[130px] xs:max-w-full max-w-[330px] sm:min-w-[70%] scale-[0.7]"
+                          src={sponsor.logo.publicURL}
+                          width={sponsor.scale}
+                          alt={sponsor.title}
+                        />
+                      )}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </li>
+        </ul>
+      </section>
+      {/* SPONSORS */}
     </Layout>
   );
 };
@@ -55,3 +135,19 @@ export default IndexPage;
 export const Head: HeadFC = ({ location: { pathname } }) => (
   <SEO pathname={pathname} />
 );
+
+export const query = graphql`
+  query Sponsors {
+    allCommunityYaml {
+      nodes {
+        id
+        title
+        url
+        logo {
+          publicURL
+        }
+        scale
+      }
+    }
+  }
+`;
